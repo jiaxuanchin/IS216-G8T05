be9222ced8069b56c219764c5118223dc6ffd4e5
JXChin02 <jxchin.2021@scis.smu.edu.sg>
2022-11-05 10:46:38 +0800

    Add CSS + files + homepage + login/signup

---
+++ firebase_group/form.js
@@ -0,0 +1,82 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword,  createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-auth.js";
+
+ const firebaseConfig = {
+  apiKey: "AIzaSyCLWmg-X8gl75JxCVz57jOIekeqSUk6XUY",
+  authDomain: "adventuretime-934a2.firebaseapp.com",
+  projectId: "adventuretime-934a2",
+  storageBucket: "adventuretime-934a2.appspot.com",
+  messagingSenderId: "168230082500",
+  appId: "1:168230082500:web:6fbd2239d875c99c467691",
+  measurementId: "G-MXMNLZZLM8",
+  databaseURL: "https://adventuretime-934a2-default-rtdb.asia-southeast1.firebasedatabase.app",
+ };
+
+ const app = initializeApp(firebaseConfig);
+ const auth = getAuth(app);
+
+
+  document.getElementById("reg-btn").addEventListener('click', function(){
+   document.getElementById("register-div").style.display="inline";
+   document.getElementById("login-div").style.display="none";
+});
+
+document.getElementById("log-btn").addEventListener('click', function(){
+ document.getElementById("register-div").style.display="none";
+ document.getElementById("login-div").style.display="inline";
+
+});
+
+  document.getElementById("login-btn").addEventListener('click', function(){
+   const loginEmail= document.getElementById("login-email").value;
+   const loginPassword =document.getElementById("login-password").value;
+
+   signInWithEmailAndPassword(auth, loginEmail, loginPassword)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    document.getElementById("result-box").style.display="inline";
+     document.getElementById("login-div").style.display="none";
+     document.getElementById("result").innerHTML="Welcome Back<br>"+loginEmail+" was Login Successfully";
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    document.getElementById("result-box").style.display="inline";
+     document.getElementById("login-div").style.display="none";
+     document.getElementById("result").innerHTML="Sorry ! <br>"+errorMessage;
+
+  });
+});
+
+
+  document.getElementById("register-btn").addEventListener('click', function(){
+
+   const registerEmail= document.getElementById("register-email").value;
+   const registerPassword =document.getElementById("register-password").value;
+
+   createUserWithEmailAndPassword(auth, registerEmail, registerPassword)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    document.getElementById("result-box").style.display="inline";
+     document.getElementById("register-div").style.display="none";
+     document.getElementById("result").innerHTML="Welcome <br>"+registerEmail+" was Registered Successfully";
+  }).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    document.getElementById("result-box").style.display="inline";
+     document.getElementById("register-div").style.display="none";
+     document.getElementById("result").innerHTML="Sorry ! <br>"+errorMessage;
+
+  });
+});
+
+
+document.getElementById("log-out-btn").addEventListener('click', function(){
+  signOut(auth).then(() => {
+     document.getElementById("result-box").style.display="none";
+       document.getElementById("login-div").style.display="inline";
+  }).catch((error) => {
+     document.getElementById("result").innerHTML="Sorry ! <br>"+errorMessage;
+  });
+
+});
